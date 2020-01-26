7b8ef3ce52e57cea0bb4d39fe7d108da9dbf047d
Maliha Nan <malihanan09@gmail.com>
2020-01-26 15:26:43 +0530

    Event live functionality added.

--- lib/edit_event.dart
+++ lib/edit_event.dart
@@ -1,113 +1,3 @@
-/*
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dsc_event_adder/event.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
-import 'package:intl/intl.dart';
-import 'dart:io';
-import 'package:path/path.dart';
-
-class EditEvent extends StatefulWidget
-{
-  Event event;
-
-  EditEvent(Event event) {
-    this.event = event;
-  }
-  @override
-  State<StatefulWidget> createState() {
-    return EditEventState();
-  }
-}
-
-class EditEventState extends State<EditEvent>
-{
-  File _image;
-
-  final formkey = GlobalKey<FormState>();
-  bool _autoValidate = false;
-  bool showImageError = false;
-  final dateformat = DateFormat("dd MMMM yyyy");
-  final timeformat = DateFormat.jm();
-  String _startTime;
-  String _endTime;
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body:new Container(
-        color: Colors.grey[300],
-        child: Center(
-          child:new Card(
-              color: Colors.white,
-              margin: EdgeInsets.all(10.0),
-              child: Form(
-                  key: formkey,
-                  child:Padding(
-                    padding: EdgeInsets.all(15.0),
-                    child: ListView(
-                      children: <Widget>[
-                        new TextFormField(
-                          initialValue: widget.event.eventName,
-                          decoration: new InputDecoration(
-                              labelText: 'Title'
-                          ),
-                          onSaved: (String value) {
-                            debugPrint(value);
-                            widget.event.eventName = value;
-                          }
-                        ),
-                        new TextFormField(
-                          initialValue: widget.event.description,
-                          keyboardType: TextInputType.multiline,
-                          maxLines: null,
-                          decoration: new InputDecoration(
-                              labelText: 'Description'
-                          ),
-                          onSaved: (String value) {
-                            widget.event.description = value;
-                          }
-                        ),
-                        new Padding(padding: EdgeInsets.all(15.0)),
-                        new RaisedButton(
-                          color: Colors.blue,
-                          textColor: Colors.white,
-                          disabledColor: Colors.grey,
-                          disabledTextColor: Colors.black,
-                          padding: EdgeInsets.all(8.0),
-                          splashColor: Colors.red,
-                          onPressed: () {
-                            formkey.currentState.save();
-                            final DocumentReference postRef = Firestore.instance.document('events/' + widget.event.id);
-                            Firestore.instance.runTransaction((Transaction tx) async {
-                              DocumentSnapshot postSnapshot = await tx.get(postRef);
-                              if (postSnapshot.exists) {
-                                await tx.update(postRef, <String, dynamic>{'title': widget.event.eventName, 'description': widget.event.description});
-                              }
-                            });
-                            Navigator.of(context).pop(this);
-                          },
-                          child: Text(
-                            "Submit",
-                            style: TextStyle(fontSize: 20.0),
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-
-              )
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dsc_event_adder/event.dart';
 import 'package:flutter/cupertino.dart';
@@ -286,6 +176,8 @@ class EditEventState extends State<EditEvent>
                           },
                         ), //totalSeats
                         TextFormField(
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
                           initialValue: widget.event.extraInfo,//"N.A.",
                           decoration: new InputDecoration(
                               hintText: 'extra information here..',
@@ -299,6 +191,8 @@ class EditEventState extends State<EditEvent>
                           },
                         ), //extraInfo
                         TextFormField(
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
                           initialValue: widget.event.what_to_bring, //"N.A.",
                           decoration: new InputDecoration(
                               hintText: 'things to bring here..',
--- lib/eventList.dart
+++ lib/eventList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dsc_event_adder/event.dart';
+import 'package:flutter/rendering.dart';
 
 class EventList extends StatelessWidget {
   @override
@@ -8,7 +9,11 @@ class EventList extends StatelessWidget {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('events').snapshots(),
       builder: (context, snapshot) {
-        if (!snapshot.hasData) return LinearProgressIndicator();
+        if (!snapshot.hasData) return Center(
+          child: CircularProgressIndicator(
+            backgroundColor: Colors.cyan[50],
+          ),
+        );
         return _buildList(context, snapshot.data.documents);
       },
     );
@@ -26,45 +31,52 @@ class EventList extends StatelessWidget {
     return Padding(
       key: ValueKey(event.eventName),
       padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),/*
-        child: Column(
-          children: <Widget>[
-            Image.network(
-              event.imageUrl,
-              height: 150,
-              fit: BoxFit.contain,
-            ),
-          ],
-        )*/
-        child: ListTile(
-          contentPadding: EdgeInsets.all(4),
-          title: Container(
-            height: 150.0,
-            child: Image.network(
-              event.imageUrl,
-              fit: BoxFit.fitWidth,
-            ),
-          ),
-          subtitle: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
-            child: Text(
-              event.eventName,
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
+      child: GestureDetector(
+        onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+          Event e = new Event.fromData(event.id, event.branch, event.currentAvailable, event.date, event.description, event.eventName, event.extraInfo, event.imageUrl, event.postedOn, event.semester, event.timings, event.totalSeats, event.venue, event.what_to_bring);
+          return e;
+        })),
+        child: Card(
+          color: Colors.grey[50],
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)),
+                child: Container(
+                  height: 150.0,
+                  width: MediaQuery.of(context).size.width - 32,
+                  child: Image.network(
+                    event.imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        event.eventName,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      event.date
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-          onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-            Event e = new Event.fromData(event.id, event.branch, event.currentAvailable, event.date, event.description, event.eventName, event.extraInfo, event.imageUrl, event.postedOn, event.semester, event.timings, event.totalSeats, event.venue, event.what_to_bring);
-            return e;
-          }))
         ),
-      ),
+      )
     );
   }
 }
